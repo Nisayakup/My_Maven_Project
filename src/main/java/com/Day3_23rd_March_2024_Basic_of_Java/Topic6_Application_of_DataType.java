56bbc328fc1018a0635f6d4ae35fdcba69b47c53
Nisayakup <164764014+Nisayakup@users.noreply.github.com>
2024-06-24 12:23:35 -0500

    Add files via upload

---
+++ src/main/java/com/Day3_23rd_March_2024_Basic_of_Java/Topic6_Application_of_DataType.java
@@ -0,0 +1,54 @@
+package com.java.Day3_23rd_March_2024_Basic_of_Java;
+
+public class Topic6_Application_of_DataType 
+{
+
+	//addition
+	//subtraction
+    //multiplication
+	//division
+	//to the power
+	//remainder
+	
+	
+	//add two numbers(integers)
+	public static void main() {
+		
+		addtwonumbers();
+	}
+	
+	
+	
+		
+		public static void addtwonumbers() { 
+			
+		
+	
+		int number1 = 100;
+		int number2 = 200;
+	
+		
+	    System.out.println(number1 + number2);
+	    System.out.println(number1 - number2);	    
+	    System.out.println(number1 % number2);
+	    System.out.println(number1 / number2);
+	    System.out.println(number1 ^ number2);
+	    System.out.println(number1 % number2);
+	    
+	    
+	    
+	    
+	    
+	    // double
+	    double d1 = 10.10;
+	    double d2 = 1.1; 
+	    System.out.println(d1 + d2 );
+	    System.out.println(d1 - d2);
+	    System.out.println(d1 * d2);
+	    System.out.println(d1 / d2);
+	    System.out.println(d1 % d2);
+	    
+		}
+	
+}
+

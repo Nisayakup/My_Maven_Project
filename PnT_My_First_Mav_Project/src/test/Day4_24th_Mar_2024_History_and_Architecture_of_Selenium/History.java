8976ff46bcbb2c54f4838f37bf7cec8d41f255d6
Nisayakup <164764014+Nisayakup@users.noreply.github.com>
2024-06-24 13:19:14 -0500

    Add files via upload

---
+++ PnT_My_First_Mav_Project/src/test/Day4_24th_Mar_2024_History_and_Architecture_of_Selenium/History.java
@@ -0,0 +1,37 @@
+package com.learning.Day4_24th_Mar_2024_History_and_Architecture_of_Selenium;
+
+public class History {
+	
+	
+	// https://selenium.org
+	
+	//Selenium IDE (Record and playback tool)
+	
+	//Selenium RC [Remote Control] - complicated 
+	
+	//Selenium WebDriver [4 is going - 4.18.1]
+	
+	//Selenium GRID [you can use multiple nodes with a single client] 
+	
+	
+	
+	// interviews :
+	
+	
+	//JavaSCRIPT / Cypress
+	//JavaScript  / WebDriver10
+	//Python  / PyTest
+	
+	
+	
+	
+	// Selenuim 2
+	
+	//selenium 3 VS Selenium 4 [Oct 2021]
+	
+	//5-7-10 years experience 
+	
+	//architecture difference between 3 and 4 
+	
+
+}
